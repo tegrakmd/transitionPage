1a8dbdaab776d126eabc539f0e54abcc36fb991e
tegrakmd <tegrakamanda@gmail.com>
2026-05-12 01:33:37 +0200

    kmd description

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
        <ReactLenis root>
         <section className="h-screen w-full bg-indigo-400">
          <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-5xl font-bold text-white">About</h1>
+            <h1 className="text-5xl font-bold text-white">About Kmd</h1>
          </div>
         </section>
        </ReactLenis>
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
 <section className="h-screen w-full bg-black">
  <div className="flex flex-col items-center justify-center h-full">
  <h1 className="text-4xl font-bold text-white">Home</h1>
-  <p className="text-white">Welcome to the home page</p>
+  <div className="max-w-2xl mx-auto px-4 mt-6">
+    <p className="text-white text-balance text-center">Tegra kmd is a software engineer frontend developer with a passion for creating beautiful and functional web applications. He is a self-taught developer and he is always learning new things.</p>
+  </div>
  
  </div>
 </section>
